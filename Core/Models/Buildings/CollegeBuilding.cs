63590ec3f55d669ca08d070f2ab54b6e6f5697bc
dnl05 <danek2005w@gmail.com>
2025-11-01 21:56:46 +0300

    1. Добавлены классы школы и уника 2. Переработана логика выпуска и учебы гражданина 3. Переделал класс Car под личный транспорт 4. Создан абстрактный класс Транспорта 5. Добавлена абстракция остановок для ОТ 6. Логика Пассажиров и Передвижения

---
+++ Core/Models/Buildings/CollegeBuilding.cs
@@ -0,0 +1,16 @@
+﻿using Core.Enums;
+
+namespace Core.Models.Buildings;
+
+public class CollegeBuilding : ServiceBuilding
+{
+    /// <summary>
+    /// Конструктор здания колледжа
+    /// </summary>
+    /// <param name="capacity">Опциональная вместимость здания</param>
+    public CollegeBuilding(int capacity = 0) 
+        : base(ServiceBuildingType.College, capacity)
+    {
+        // Здесь можно добавить любую специфичную для школы логику или свойства
+    }
+}
