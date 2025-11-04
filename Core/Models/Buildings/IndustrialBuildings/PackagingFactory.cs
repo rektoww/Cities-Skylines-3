e9aa6491603ca06f64e23b357d120be92c9c61e2
Andrey <awentus2528@bk.ru>
2025-11-04 16:13:41 +0300

    Create IndustrialBuildings
    Create IndustrialBuildings

---
+++ Core/Models/Buildings/IndustrialBuildings/PackagingFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Models.Buildings.IndustrialBuildings
+{
+    /// <summary>
+    /// Фабрика по производству упаковки
+    /// </summary>
+    public class PackagingFactory
+    {
+        #region Enums - Типы материалов и продукции
+        
+    }
+}
