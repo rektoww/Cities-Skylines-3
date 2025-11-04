6d8926491292c38a1118b9177d8dd4bd0be4cb6f
zxcmaxzxc <wirelessgtx@mail.ru>
2025-11-04 17:44:54 +0300

    Create FisheryType.cs

---
+++ Core/Enums/FisheryType.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    public enum FisheryType
+    {
+        // Флот и добыча
+        FishingBoat,
+        FishingTrawler,
+        FishingBase,
+        // Хранение
+        FishStorage,
+        RefrigeratedWarehouse 
+    }
+}
