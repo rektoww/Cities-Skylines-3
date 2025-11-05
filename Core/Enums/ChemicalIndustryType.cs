94d24059c22ecd1fcb380b864795aa0ef11e3cf4
Николай <brawlstarsofficial@gmail.com>
2025-11-06 01:00:46 +0300

    Реализовал модель сферы логистики и химической промышленности

---
+++ Core/Enums/ChemicalIndustryType.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    /// <summary>
+    /// Типы химических производств
+    /// </summary>
+    public enum ChemicalIndustryType
+    {
+        // Основная химия
+        PetrochemicalPlant,
+        FertilizerPlant,
+        PolymerPlant,
+        AcidProduction,
+        BaseChemicals,
+
+        // Специализированная химия
+        PharmaceuticalPlant,
+        PaintAndVarnish,
+        CosmeticsChemical,
+        Agrochemicals,
+        CleaningChemicals,
+
+        // Высокотехнологичная химия
+        FineChemicals,
+        SpecialtyChemicals,
+        Biotechnology,
+        Nanomaterials
+    }
+
+    /// <summary>
+    /// Типы химического сырья
+    /// </summary>
+    public enum ChemicalMaterial
+    {
+        // Основное сырье
+        CrudeOil,
+        NaturalGas,
+        Coal,
+        Salt,
+        Sulfur,
+        Phosphates,
+        Limestone,
+
+        // Химические реагенты
+        Ammonia,
+        Chlorine,
+        Hydrogen,
+        Oxygen,
+        Nitrogen,
+        SulfuricAcid,
+        NitricAcid,
+
+        // Органические соединения
+        Ethylene,
+        Propylene,
+        Benzene,
+        Methanol,
+        Ethanol
+    }
+
+    /// <summary>
+    /// Типы химической продукции
+    /// </summary>
+    public enum ChemicalProduct
+    {
+        // Основные химикаты
+        Ammonia,
+        Urea,
+        SulfuricAcid,
+        NitricAcid,
+        CausticSoda,
+        SodaAsh,
+
+        // Удобрения
+        NitrogenFertilizers,
+        PhosphorusFertilizers,
+        PotassiumFertilizers,
+        ComplexFertilizers,
+
+        // Полимеры и пластмассы
+        Polyethylene,
+        Polypropylene,
+        PVC,
+        Polystyrene,
+        SyntheticRubber,
+
+        // Специальные химикаты
+        Pharmaceuticals,
+        Paints,
+        Detergents,
+        Cosmetics,
+        Agrochemicals,
+
+        // Промежуточные продукты
+        Methanol,
+        Ethylene,
+        Propylene,
+        Benzene
+    }
+
+    /// <summary>
+    /// Типы химических процессов
+    /// </summary>
+    public enum ChemicalProcess
+    {
+        Distillation,
+        Cracking,
+        Polymerization,
+        Hydrogenation,
+        Oxidation,
+        Neutralization,
+        Electrolysis,
+        Fermentation,
+        Crystallization,
+        Filtration
+    }
+}
---
+++ Core/Enums/LogisticsType.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    /// <summary>
+    /// Типы логистических компаний и услуг
+    /// </summary>
+    public enum LogisticsType
+    {
+        // По типу транспорта
+        TruckingCompany,
+        RailwayLogistics,
+        AirCargo,
+        MaritimeShipping,
+        CourierServices,
+
+        // По специализации
+        RefrigeratedLogistics,
+        BulkCargo,
+        ContainerShipping,
+        ExpressDelivery,
+        WarehouseLogistics,
+
+        // По охвату
+        LocalLogistics,
+        RegionalLogistics,
+        InternationalLogistics
+    }
+
+    /// <summary>
+    /// Типы перевозимых грузов в логистике
+    /// </summary>
+    public enum CargoType
+    {
+        GeneralCargo,
+        PerishableGoods,
+        DangerousGoods,
+        OversizedCargo,
+        FragileGoods,
+        LiquidCargo,
+        BulkMaterials,
+        RefrigeratedGoods,
+        LiveAnimals,
+        Documents,
+        HighValueGoods
+    }
+
+    /// <summary>
+    /// Типы транспортных средств в логистике
+    /// </summary>
+    public enum LogisticsVehicle
+    {
+        Truck,
+        RefrigeratedTruck,
+        TankerTruck,
+        FlatbedTruck,
+        ContainerTruck,
+
+        DumpTruck,
+        CarCarrier,
+        LivestockTruck,
+
+        CargoPlane,
+        Helicopter,
+
+        CargoShip,
+        ContainerShip,
+        Tanker
+    }
+}
