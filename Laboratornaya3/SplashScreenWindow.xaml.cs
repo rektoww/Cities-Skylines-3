9d32c8cf128a65c916b9e73d44788f705ff889ca
Максим Максим <party.maker.max@mail.ru>
2025-11-07 21:24:00 +0300

    Залил вместе с музыкой и заставкой Добавил цену парку Затраты ресурсов для парка

---
+++ Laboratornaya3/SplashScreenWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Laboratornaya3.Views
+{
+    public partial class SplashScreenWindow : Window
+    {
+        public SplashScreenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
