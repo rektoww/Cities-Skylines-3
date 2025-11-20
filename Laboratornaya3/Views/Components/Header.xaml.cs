7e8dfef176199094f6b6d87f2f7934e6db6676bb
dnl05 <danek2005w@gmail.com>
2025-11-20 15:52:38 +0300

    Разбит MainWindow на компоненты

---
+++ Laboratornaya3/Views/Components/Header.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Laboratornaya3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratornaya3.Views.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для Header.xaml
+    /// </summary>
+    public partial class Header : UserControl
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public Header()
+        {
+            InitializeComponent();
+        }
+
+        // исправить привязку
+        private void ClearBuildings_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("допилить");
+        }
+
+        // тоже исправить привязку
+        private void BudgetPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ViewModel?.GameState.ShowFinanceInfoCommand.Execute(null);
+        }
+    }
+}
---
+++ Laboratornaya3/Views/Components/Legend.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratornaya3.Views.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для Legend.xaml
+    /// </summary>
+    public partial class Legend : UserControl
+    {
+        public Legend()
+        {
+            InitializeComponent();
+        }
+
+        private void LegendButton_Click(object sender, RoutedEventArgs e)
+        {
+            LegendPanel.Visibility = LegendPanel.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+    }
+}
